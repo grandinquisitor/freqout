1c0f6b1959ccf70018bde385ee648ee57fbed79d
Nicholas Pisarro <infinityminusnine@gmail.com>
2025-12-23 21:52:03 -0500

    PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  './icon/icon-192.png',
+  './icon/icon-512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Strategy: Cache First, then Network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
